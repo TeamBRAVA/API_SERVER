0fb6d5ec0639b2da295c4a62b179e9306d1e2651
uncoolshogoon <paulletilly@gmail.com>
2016-04-03 17:57:03 +0200

    red_modules/red-devices/index.js function validateNewSoftware should work

--- routes/devices.js
+++ routes/devices.js
@@ -552,7 +552,7 @@ router.post('/ack/:idsoft', function(req, res) {
     //Create the object containing fields to search for
     var device = {
         id: req.device.id,
-        validateSoft: req.param.namesoft,
+        validateSoft: req.param.idsoft,
     }
     //we call devices data function that will take, the object, translate it into model object and then save it
     devices.validateNewSoftware(device, callback);
